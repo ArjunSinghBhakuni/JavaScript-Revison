79d53e626db666926a24d9380f481e447eccbf32
ArjunSinghBhakuni <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-08-10 01:24:49 +0530

    day-1

---
+++ basic-js-10-Aug/scope_2.js
@@ -0,0 +1,21 @@
+// let keyword
+
+// declare variable with let keyword
+
+// cannot redeclare block-scoped varibale "first_name"
+
+let first_name ="arjun"
+// let first_name = " suraj"
+console.log(first_name)
+
+// why let 
+// block scope  vs functional scope
+
+
+
+// constant -> const
+// we cannot change const value
+
+const pi =3.14
+// pi=3.15  Assignment to constant variable.
+console.log(pi)
---
+++ basic-js-10-Aug/variable_1.js
@@ -0,0 +1,34 @@
+// for compulsory using var in js
+"use strict";
+
+// use strict gives us - agr hum var declare nhi krenge tho - ReferenceError: firstName is not defined 
+
+//intro variables
+
+// 1. varibale can store some information
+// 2. we can use that information later
+// 3 we can change that informationlater 
+
+// declare varibale
+
+  var firstName = "Arjun singh"
+
+console.log(firstName)
+
+// change 
+ firstName = "suraj"
+ console.log(firstName)
+
+ // Rules for naming variable
+
+ //1 you cannot start with number
+
+ // 1value - invaild
+ // valule - valid
+ //_firstname - valid
+ //$fistname - valid
+
+ // first name - invaild
+ 
+// first_name - valid  snake_case
+ //firstName - valid    camalCase
