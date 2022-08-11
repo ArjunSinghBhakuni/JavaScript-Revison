d769298176de57625fa1cf032c39d2d5d4edc920
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-11 10:04:32 +0530

    day2

---
+++ basic-js-10-Aug/4.js
@@ -0,0 +1,25 @@
+// undefined // null
+
+
+// var let 
+// value not assigned to get undefined 
+
+// in case of const  this become a referrence error
+console.log(typeof undefined)
+
+
+console.log(typeof null) 
+
+// type of null object 
+// because of bug, error 
+
+
+//bigInit
+// for big number
+
+let myNumber = BigInt(34534534523453245345345)
+let sameNumber= 3432432432432432432n
+console.log(myNumber)
+
+
+// boolean & comparison operator
