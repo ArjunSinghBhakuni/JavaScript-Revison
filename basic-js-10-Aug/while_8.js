af7c2d7ef0c81975320ce1fd57fddd5dda5d8ee0
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 06:23:26 +0530

    whileloop day4

---
+++ basic-js-10-Aug/while_8.js
@@ -0,0 +1,18 @@
+// while loop
+
+
+// dry -> dont repeat yourself
+// rather than doing to many times we can use loops
+
+// it take linear time
+let i=0;
+while(i<=3){
+ console.log(i)
+ i++
+}
+console.log(i)
+
+
+// sum of n natural number
+
+ (n*(n+1))/2 // it take constant  time
