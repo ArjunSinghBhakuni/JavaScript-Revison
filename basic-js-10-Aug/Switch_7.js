b424aa24cc06a848b090b91ab7ec871178643d12
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 06:13:35 +0530

    swtich_7 day4

---
+++ basic-js-10-Aug/Switch_7.js
@@ -0,0 +1,33 @@
+// switch statement
+
+
+let day = 0;
+
+switch(day){
+case 0:
+ console.log("Sunday");
+ break;
+ case 1:
+ console.log("Monday");
+ break;
+ case 2:
+ console.log("Tuesday");
+ break;
+ case 3:
+ console.log("Wednesday");
+ break;
+ case 4:
+ console.log("Thusday");
+ break;
+ case 5:
+ console.log("Friday");
+ break;
+ case 6:
+ console.log("Saturday");
+ break;
+ 
+ default:
+ console.log("invalid day")
+
+
+}
