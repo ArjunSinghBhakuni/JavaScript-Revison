7543b2144bf6747bb7003fb3f62961c1704bdb59
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 06:07:06 +0530

    NestedufElse day4

---
+++ basic-js-10-Aug/nestedIfElse6.js
@@ -0,0 +1,22 @@
+// nested if else
+
+
+let winning = 19;
+let userGuess = +prompt("Guess a  number")
+
+if(userGuess === winning){
+ console.log("Your guess is right")
+} 
+else {
+ if(userGuess<winning){
+  console.log("Your guess is two low")
+ }else{
+  console.log("you guess is high")
+ }
+ 
+}
+
+
+// if 
+// else if 
+// else
