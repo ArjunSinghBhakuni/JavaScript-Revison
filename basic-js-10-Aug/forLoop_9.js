2b8bdb2cd23e8f457a77bf6d77bb16298e0a3f23
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 06:27:47 +0530

    forLoop_9 day4

---
+++ basic-js-10-Aug/forLoop_9.js
@@ -0,0 +1,13 @@
+// for loop
+
+for(let i=0;i<=9;i++){
+ console.log(i)
+}
+
+// console.log(i) is not define
+
+
+for(var i=0;i<=9;i++){
+ console.log(i)
+}
+// console.log(i) we can access outside
