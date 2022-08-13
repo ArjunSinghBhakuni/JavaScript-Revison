eb220eb807e6d72e43275d8262c52234342f8421
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 05:56:44 +0530

    day4

---
+++ basic-js-10-Aug/ifE;se5.js
@@ -0,0 +1,31 @@
+// truthy and falsy value
+
+
+
+// falsy value
+// false
+// ""
+// null 
+// undefined
+// 0
+
+let firstName = "asdf"
+
+if(firstName){
+ console.log(firstName)
+ 
+}else {
+ console.log("not name is here")
+}
+
+// terniary operator autdion
+
+
+
+let age = 8
+let drink = age>=10 ? console.log("coffie")  : console.log("milk")
+
+
+// Operators 
+
+// &&      Or(||)
