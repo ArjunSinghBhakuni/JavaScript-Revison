e61c36c31e13dda58be54d37a83af2140365bcaf
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 06:31:45 +0530

    keywird_10 day4

--- basic-js-10-Aug/keyword_10.js
+++ basic-js-10-Aug/keyword_10.js
@@ -1,6 +1,24 @@
-
+let i =0
 
 // break 
+// break will break things
 
+while(i<9){
+ if(i===4){
+break;
+ }
+ i++
+ console.log(i)
+}
 
 // continue
+
+// continue will print every thing expect 4 
+
+while(i<9){
+ if(i===4){
+continue;
+}
+i++
+ console.log(i)
+}
