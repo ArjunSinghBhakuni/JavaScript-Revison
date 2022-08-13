763c9e4017875edee471d6c0518ce000e918d224
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-14 00:07:19 +0530

    how to clone arr day5

--- basic-js-10-Aug/primitveVSrefernce.js
+++ basic-js-10-Aug/primitveVSrefernce.js
@@ -13,10 +13,20 @@ console.log(num1)
 // refernce type
 
 let array1 = ["item1","item2"]
-let array2 = array1;
+// let array2 = array1;
 
 
 array1.push("item3")
 
 console.log(array1)
-console.log(array2)
+//console.log(array2)
+
+
+// how to clone a array
+
+
+// let array2 = array1.slice(0).concat(["item3","item4"])
+// let array2 = [].concat(array1,["item3","item4"])
+let array2 = [...array1,"item3","item4"]
+
+
