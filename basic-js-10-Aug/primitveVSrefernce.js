26bb52857efd210eff958ce30a0fa0545e43b6a8
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-13 13:45:27 +0530

    primitive&reference data type day4

---
+++ basic-js-10-Aug/primitveVSrefernce.js
@@ -0,0 +1,22 @@
+// primiatvie vs reference datatype
+
+
+// primiative
+let num1=  6
+let num2 = num1
+
+num1++
+console.log(num1)
+console.log(num1)
+
+
+// refernce type
+
+let array1 = ["item1","item2"]
+let array2 = array1;
+
+
+array1.push("item3")
+
+console.log(array1)
+console.log(array2)
